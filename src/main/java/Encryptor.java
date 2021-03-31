b353ad3517d9686f3314dd3e1d0ed5f3d7e9eabf
koma-4 <koma_ni@mail.ru>
2021-03-31 17:12:43 +0300

    Исправления

--- src/main/java/Encryptor.java
+++ src/main/java/Encryptor.java
@@ -15,34 +15,38 @@ public class Encryptor {
     }
 
     /* одинаковый алгоритм для шифрования и дешифрования. Заданная по умолчанию кодировка UTF-8. */
-    private byte[] encrypt(InputStream in, OutputStream out) throws IOException {
+    private void encrypt(InputStream in, OutputStream out) throws IOException {
         try (DataInputStream input = new DataInputStream(in)) {
             try (DataOutputStream output = new DataOutputStream(out)) {
                 BigInteger keyToInt = new BigInteger(key, 16);
                 byte[] keyToBytes = keyToInt.toByteArray();
-                byte[] txt = input.readAllBytes();
-                byte[] res = new byte[txt.length];
-                for (int i = 0; i < txt.length; i++) {
-                    res[i] = (byte) (txt[i] ^ keyToBytes[i % keyToBytes.length]);
+                int sym = input.readByte();
+                int count = 0;
+                while (sym != -1) {
+                    output.writeByte(sym ^ keyToBytes[count % keyToBytes.length]);
+                    count++;
+                    try {
+                        sym = input.readByte();
+                    } catch (IOException e) {
+                        break;
+                    }
                 }
-                output.write(res);
-                return res;
             }
         }
     }
 
 
-    public byte[] encrypt(String inputName, String outputName) throws IOException {
+    public void encrypt(String inputName, String outputName) throws IOException {
         try (FileInputStream inputStream = new FileInputStream(inputName)) {
             try (FileOutputStream outputStream = new FileOutputStream(outputName)) {
-                return encrypt(inputStream,outputStream);
+                encrypt(inputStream,outputStream);
             }
         }
     }
 
 
 
-    public byte[] decrypt(String inputName, String outputName) throws IOException {
-        return encrypt(inputName,outputName);
+    public void decrypt(String inputName, String outputName) throws IOException {
+        encrypt(inputName,outputName);
     }
 }
--- src/main/java/EncryptorLauncher.java
+++ src/main/java/EncryptorLauncher.java
@@ -25,21 +25,30 @@ public class EncryptorLauncher {
     private void launch(String[] args) {
         CmdLineParser parser = new CmdLineParser(this);
         String key;
-        boolean flag;
-        if (keyToDecrypt == null && keyToEncrypt != null) {
-            key = keyToEncrypt;
-            flag = true;
+        enum Check {encrypt(true), decrypt(false);
+            private final boolean value;
+            Check (boolean value) {
+                this.value = value;
+            }
+            boolean getValue() {
+                return this.value;
+            }
         }
-        else if (keyToEncrypt == null && keyToDecrypt != null) {
-            key = keyToDecrypt;
-            flag = false;
-        }
-        else  {
-            System.err.println("Error entering arguments");
-            throw new IllegalArgumentException("Illegal arguments");
-        }
+        Check toDo;
         try {
             parser.parseArgument(args);
+            if (keyToDecrypt == null && keyToEncrypt != null) {
+                key = keyToEncrypt;
+                toDo = Check.encrypt;
+            }
+            else if (keyToEncrypt == null && keyToDecrypt != null) {
+                key = keyToDecrypt;
+                toDo = Check.decrypt;
+            }
+            else  {
+                System.err.println("Error entering arguments");
+                throw new IllegalArgumentException("Illegal arguments");
+            }
             if (!key.matches("[0-9a-fA-F]+")) {
                 System.err.println("Error entering arguments");
                 throw new IllegalArgumentException("Illegal arguments");
@@ -53,7 +62,7 @@ public class EncryptorLauncher {
         }
         Encryptor encryptor = new Encryptor(key);
         try {
-            if (flag) encryptor.encrypt(inputFileName, outputFileName);
+            if (toDo.getValue()) encryptor.encrypt(inputFileName, outputFileName);
             else encryptor.decrypt(inputFileName, outputFileName);
         } catch (IOException e) {
             System.err.println(e.getMessage());
